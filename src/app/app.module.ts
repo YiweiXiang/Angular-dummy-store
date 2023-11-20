447512cb6ddc358f3f588c7fd030445efc490dcf
YiweiX <yiwei.xiang0123@gmail.com>
2023-11-20 17:59:51 -0600

    updated product to cart feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,9 @@ import { HomeComponent } from './pages/home/home.component';
 import { ProductsHeaderComponent } from './pages/home/components/products-header/products-header.component';
 import { FiltersComponent } from './pages/home/components/filters/filters.component';
 import { ProductBoxComponent } from './pages/home/components/product-box/product-box.component';
+import { CartComponent } from './pages/cart/cart.component';
+// import { CartService } from './services/cart.service';
+import { CartService } from 'src/app/services/cart.service';
 
 @NgModule({
   declarations: [
@@ -32,6 +35,7 @@ import { ProductBoxComponent } from './pages/home/components/product-box/product
     ProductsHeaderComponent,
     FiltersComponent,
     ProductBoxComponent,
+    CartComponent,
   ],
   imports: [
     BrowserModule,
@@ -50,7 +54,7 @@ import { ProductBoxComponent } from './pages/home/components/product-box/product
     MatBadgeModule,
     MatSnackBarModule,
   ],
-  providers: [],
+  providers: [CartService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
